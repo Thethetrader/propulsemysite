41865d01b0582599af01318d3dd8566f1e02656c
Theodore Brey <theodorebrey@MacBook-Pro-de-Theodore-2.local>
2025-07-07 23:05:35 +0200

    chore: expose ADMIN_PASSWORD env to Next runtime

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig 
+module.exports = nextConfig
+
+module.exports.env = {
+  ADMIN_PASSWORD: process.env.ADMIN_PASSWORD
+} 
